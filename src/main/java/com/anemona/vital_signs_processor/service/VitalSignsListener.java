b177ac40ccf9619a70ba15c41af406bf863085f6
Jayirong <lemonsoft5555@gmail.com>
2025-02-17 13:45:35 -0300

    prueba para recibir estados vitales

--- src/main/java/com/anemona/vital_signs_processor/service/VitalSignsListener.java
+++ src/main/java/com/anemona/vital_signs_processor/service/VitalSignsListener.java
@@ -1,6 +1,17 @@
 package com.anemona.vital_signs_processor.service;
 
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.anemona.vital_signs_processor.dto.EstadoVitalDTO;
+
 // Escucha kafka y procesa datos
+@Service
 public class VitalSignsListener {
     
+    @KafkaListener(topics = "senales_vitales", groupId = "vital-signs-group")
+    public void listen(EstadoVitalDTO estadoVital) {
+        System.out.println("RESIVIDOCCCC: " + estadoVital);
+    }
+
 }
