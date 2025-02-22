9c8e4e8b151464da772ff9abebe9d5a30ecd0403
Jayirong <lemonsoft5555@gmail.com>
2025-02-22 16:27:18 -0300

    build

--- src/main/java/com/anemona/vital_signs_processor/service/AlertService.java
+++ src/main/java/com/anemona/vital_signs_processor/service/AlertService.java
@@ -4,8 +4,10 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.anemona.vital_signs_processor.dto.AlertaDTO;
 import com.anemona.vital_signs_processor.dto.EstadoVitalDTO;
@@ -16,6 +18,11 @@ public class AlertService {
     
     @Autowired
     private KafkaTemplate<String, AlertaDTO> kafkaTemplate;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private static final String ANEBACK_URL = "http://aneback:8080/api/estadoVitales/ingreso/";
 
     private static final String ALERTAS_TOPIC = "alertas";
 
@@ -39,6 +46,24 @@ public class AlertService {
     //si no existe un id para el estado vital nos sacamos uno del poto
     private Long generarIdEstadoVital() {
         return System.currentTimeMillis(); 
+    }
+
+    //metodo para procesar el estado vital a OCI
+    public void procesarAlerta(EstadoVitalDTO estadoVital, Long pacienteId) {
+
+        String url = ANEBACK_URL + pacienteId;
+
+        try {
+            ResponseEntity<EstadoVitalDTO> response = restTemplate.postForEntity(url, estadoVital, EstadoVitalDTO.class);
+            if (response.getStatusCode().is2xxSuccessful()) {
+                System.out.println("Estado vital registrado correctamente en aneback.");
+            } else {
+                System.err.println("Error al registrar el estado vital en Aneback. Cod: " + response.getStatusCode());
+            }
+        } catch (Exception e) {
+            System.err.println("Error al comunicarse con aneback: " + e.getMessage());
+        }
+
     }
 
 }
